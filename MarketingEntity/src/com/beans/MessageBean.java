0f98e4cec08a7aab7c10f3bdcbdaa8a86ac30a0f
Sagar <sagar.gurav@webaccessglobal.com>
2018-01-24 17:43:20 +0530

    Added message bean and sent messages bean

---
+++ MarketingEntity/src/com/beans/MessageBean.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.beans;
+
+/**
+ *
+ * @author sagar.gurav
+ */
+public class MessageBean {
+
+    private int id;
+    private String subject;
+    private String body;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
